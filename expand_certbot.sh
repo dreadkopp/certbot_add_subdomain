4e8b4e681ea05c73ab69ffbbc1f5165ec786bd3c
Arne Ziegert <arne.ziegert@gmx.de>
2018-11-04 13:23:54 +0100

    run either interactively or with arguments

--- expand_certbot.sh
+++ expand_certbot.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-certnames=$(certbot certificates | grep "Certificate Name:")
-printf "Found the following Certificates:\n\n${certnames}\n\nPlease enter the name of the certificate you want to expand and press ENTER \n"
-read certname
+certname=$1
+newdomain=$2
+interactive=0
+
+
+if [ -z "$newdomain" ]
+    then
+	interactive=1
+fi
+
+
+#if interactive ask for cert name
+if [ $interactive -eq "1" ]
+    then
+	certnames=$(certbot certificates | grep "Certificate Name:")
+	printf "Found the following Certificates:\n\n${certnames}\n\nPlease enter the name of the certificate you want to expand and press ENTER \n"
+	read certname
+fi
 
 #get currently existing domains
 existing=$(certbot certificates | grep -A 1 "Name: ${certname}" | grep Domains);
@@ -22,9 +37,18 @@ existing=${existing#*:}
 existing=$(echo ${existing} | xargs)
 #replace spaces with comma
 existing=${existing// /,}
-printf "These are the current domains for the certificate ${certname}: \n${existing}\n\n";
-echo "Please enter the new domain you want to add to that certificate:"
-read newdomain
+
+#if interactive ask for new domain
+if [ $interactive -eq "1" ]
+    then
+	printf "These are the current domains for the certificate ${certname}: \n${existing}\n\n";
+	echo "Please enter the new domain you want to add to that certificate:"
+	read newdomain
+fi
+
 alldomains="${existing},${newdomain}"
 
+echo $alldomains
+
+#start certbot
 certbot certonly --cert-name ${certname} -d ${alldomains}
